8fda147f7a93386a3311d79ae358676a66951014
wanglin <903555096@qq.com>
2019-07-08 15:00:11 +0800

    hhh

--- src/test/java/com/springboot/cache/ElasticsearchApplicationTests.java
+++ src/test/java/com/springboot/cache/ElasticsearchApplicationTests.java
@@ -12,7 +12,6 @@ import io.searchbox.core.SearchResult;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.actuate.health.ElasticsearchHealthIndicator;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
